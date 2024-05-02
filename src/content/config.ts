dcfc34048b67f7ae592493e30004d634086192de
Kara-Zor-El <kara.wilson.2005.08@gmail.com>
2024-05-01 22:12:19 -0400

    astro learning test

---
+++ src/content/config.ts
@@ -0,0 +1,12 @@
+import { defineCollection, z } from "astro:content";
+
+const postsCollection = defineCollection({
+  schema: ({ image }) => z.object({
+    author: z.string(),
+    date: z.string(),
+    image: image(),
+    title: z.string(),
+  }),
+});
+
+export const collections = { posts: postsCollection };
